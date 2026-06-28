2c103bda76aa757960eebd84567fa96bc9c8b71a
shivam_raj <shivamraj81020051@gmail.com>
2026-06-28 16:04:22 +0530

    Transactions

---
+++ day123.sql
@@ -0,0 +1,16 @@
+-- ✅ Day 123 — Transactions Interview Questions
+
+-- Concepts:
+-- ACID
+-- Isolation levels
+-- Dirty read
+-- Phantom read
+
+-- Practice:
+-- Explain isolation levels with example.
+-- SQL:
+-- Transaction example with rollback.
+
+-- Bank Account Summary II
+-- Customers Who Visited but Did Not Make Any Transactions
+-- Average Selling Price
